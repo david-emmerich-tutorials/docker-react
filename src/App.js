3ce635e2c9ce06e5c908986164238ca6b168e02b
David Emmerich <david.emmerich@outlook.de>
2021-04-09 17:28:42 +0200

    bugfix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>>Another commit to test the pipe</p>
+        <p>bugfix</p>
         <a
           className="App-link"
           href="https://reactjs.org"
